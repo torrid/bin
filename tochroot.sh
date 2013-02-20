d40ba023fda640154f60c3c84970c647a1ab3926
Torrid Luna <torrid@virtyou.com>
2013-02-20 20:24:00 +0100

    commented out my special folders for portage and kernels

--- tochroot.sh
+++ tochroot.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+
+# This goes to the rootfolder of any chroot environment. Simply call the script (as root)
+# from the host OS, it determines it's own root folder and mounts / binds all 
+# necessary and convenient folders into the chroot. 
+
 me=$(readlink -f "$0" )
 mypath=$(dirname "$me")
 
 echo "# $me - $mypath" 
 
+
+# mount | grep -q "/usr/src" || mount /dev/Schlonz/Kernels /usr/src/
+# mount --bind /usr/src/ $mypath/usr/src/
+# mount | grep -q "var/portage" || mount /dev/Virtuals/Portage $mypath/var/portage
+
 mkdir -p $mypath/dev $mypath/sys $mypath/proc
-
-mount | grep -q /dev/Schlonz/Kernels || mount /dev/Schlonz/Kernels /usr/src/
-mount --bind /usr/src/ $mypath/usr/src/
-
-mount | grep -q /dev/Virtuals/Portage || mount /dev/Virtuals/Portage $mypath/var/portage
 
 chroot $mypath /bin/mount -t proc none /proc
 chroot $mypath /bin/mount -t sysfs none /sys
